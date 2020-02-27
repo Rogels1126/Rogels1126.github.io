f205a1ceb58e8680ef479f173875f04dd7b8749a
Rogels1126 <55113168+Rogels1126@users.noreply.github.com>
2020-02-27 15:24:14 -0500

    Add files via upload

---
+++ Clocknimation/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+  setTimeout(function() {
+    $(".box").addClass("alarm");
+  }, 80000);
+});;
+
+.ready(function(){
+  
+  setTimeout(function() {
+    $(".G1B").addClass("alarm");
+  }, 80000);
+});;
