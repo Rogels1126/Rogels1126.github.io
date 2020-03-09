0bcfbc777a9620111cc3a392457b225943660a60
Rogels1126 <55113168+Rogels1126@users.noreply.github.com>
2020-03-09 15:34:25 -0400

    Add files via upload

--- Clocknimation/script.js
+++ Clocknimation/script.js
@@ -1,13 +1,32 @@
 $(document).ready(function(){
-  
   setTimeout(function() {
-    $(".box").addClass("alarm");
-  }, 80000);
-});;
+	 $(".box").addClass("alarm");
+    $(".O1B").addClass("alarm");
+    $(".O2B").addClass("alarm");
+    $(".O3B").addClass("alarm");
+    $(".O4B").addClass("alarm");
+     $(".O5B").addClass("alarm");
+       $(".O6B").addClass("alarm");
+     $(".O7B").addClass("alarm");
+ $(".O8B").addClass("alarm");
 
-.ready(function(){
-  
-  setTimeout(function() {
-    $(".G1B").addClass("alarm");
-  }, 80000);
-});;
+ $(".G1B").addClass("alarm");
+    $(".G2B").addClass("alarm");
+    $(".G3B").addClass("alarm");
+    $(".G4B").addClass("alarm");
+     $(".G5B").addClass("alarm");
+
+    $(".B1B").addClass("alarm");
+    $(".B2B").addClass("alarm");
+    $(".B3B").addClass("alarm");
+    $(".B4B").addClass("alarm");
+     $(".B5B").addClass("alarm");
+
+     $(".LOCK").addClass("alarm");
+
+        $(".Warning").addClass("alarm");
+        
+        $(".Press").addClass("alarm");
+
+  }, 13000);
+});
